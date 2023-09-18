64bee13f4f3f19d892df9947f163c5aabec39306
Aurélien Roux <aurelien.roux@busbud.com>
2023-09-18 19:03:51 -0400

    add misc router mini app

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const path = require("path");
 
+const misc = require("./misc");
+
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/misc", misc);
 
 app.get("/", (req, res) => {
   res.send("hello me");
 });
 
-app.get("/users", (req, res) => {
-  res.send("users page");
-});
+app.get(
+  "/users",
+  (req, res, next) => {
+    console.log("req in first");
+    next();
+  },
+  (req, res) => {
+    res.send("users page");
+  }
+);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
